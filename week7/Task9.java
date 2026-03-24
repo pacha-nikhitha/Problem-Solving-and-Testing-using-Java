ee7c62009be966f399c39f1b1cfd90867c6a5e9b
Pacha Nikhitha <pachanikhitha2004@gmail.com>
2026-03-24 11:33:18 +0530

    Create Task9.java

---
+++ week7/Task9.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Generic method
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Predefined arrays (as per HackerRank)
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
