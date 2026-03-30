666b41e3e4d72b88618326367689b69188c3e0a6
Pacha Nikhitha <pachanikhitha2004@gmail.com>
2026-03-30 09:44:45 +0530

    Add Task11.java to count multiples of k

---
+++ SYLLABUS/Task11.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Task11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x % k == 0)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
